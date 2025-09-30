132c286a680378861a010e1bf3fa1fdd386fceaa
c0a2411932 <c0a2411932@edu.teu.ac.jp>
2025-09-30 14:53:17 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,9 +13,6 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def check_bound(rct):
-    if rct.left < 0 or WIDTH < rct.right:
-        yoko = False
 
 def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
     yoko,tate =True, True
@@ -33,25 +30,31 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+
     bb_img = pg.Surface((20,20))
+    bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img,(255, 0, 0),(10, 10), 10)
-    bb_img = set_colorkey(0, 0, 0)
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centerx = random.randint(0, HEIGHT)
     vx,vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
         screen.blit(bg_img, [0, 0])
+
         if kk_rct.colliderect(bb_rct):
             return 
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+
         for key, mv in DELTA.items(): 
             if key_lst[key]:
                 sum_mv[0] += mv[0]
@@ -65,16 +68,23 @@ def main():
         if key_lst[pg.K_RIGHT]:
             sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):
+        yoko, tate = check_bound(kk_rct)
+        if not yoko or not tate:
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
         screen.blit(kk_img, kk_rct)
+
         bb_rct.move_ip(vx, vy)
-        yoko, tate check_bound(bb_rct)
+        
+        yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1 
         if not tate:
             vy *= -1
         screen.blit(bb_img, bb_rct)
+        if kk_rct.colliderect(bb_rct):
+            return
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
